6d1383a00bd90990ccfcb0563f5c0d991558ae5a
lewjae <lewjae@gmail.com>
2020-06-30 16:41:05 -0700

    initial commitment

---
+++ object detection/cpp/build/CMakeFiles/ZED_Object_Detection.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ZED_Object_Detection.dir/src/TrackingViewer.o"
+  "CMakeFiles/ZED_Object_Detection.dir/src/main.o"
+  "CMakeFiles/ZED_Object_Detection.dir/src/GLViewer.o"
+  "ZED_Object_Detection.pdb"
+  "ZED_Object_Detection"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ZED_Object_Detection.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
